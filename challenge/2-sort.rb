c259461dbeb617d01687cab88310fc69d5e9f602
Your Name <you@example.com>
2025-09-24 22:36:43 +0300

    task 1,2 and 3

---
+++ challenge/2-sort.rb
@@ -0,0 +1,34 @@
+###
+#
+#  Sort integer arguments (ascending)
+#
+###
+
+result = []
+ARGV.each do |arg|
+  # skip if not integer
+  next unless arg =~ /\A-?\d+\z/
+
+  # convert to integer
+  i_arg = arg.to_i
+
+  # insert result at the right position
+  is_inserted = false
+  i = 0
+  l = result.size
+  while !is_inserted && i < l
+    if result[i] < i_arg
+      i += 1
+    else
+      # fix: insert at i (مو i - 1)
+      result.insert(i, i_arg)
+      is_inserted = true
+      break
+    end
+  end
+
+  # لو ما لقينا مكان (كل العناصر أصغر)، نحطه في الأخير
+  result << i_arg unless is_inserted
+end
+
+puts result
