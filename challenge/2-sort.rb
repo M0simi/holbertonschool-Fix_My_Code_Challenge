578d990de825fbf353154eaad81f19577f4adf47
Your Name <you@example.com>
2025-09-24 22:54:30 +0300

    edit

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -20,7 +20,7 @@ ARGV.each do |arg|
     if result[i] < i_arg
       i += 1
     else
-      # fix: insert at i (مو i - 1)
+      # fix: insert at i
       result.insert(i, i_arg)
       is_inserted = true
       break
