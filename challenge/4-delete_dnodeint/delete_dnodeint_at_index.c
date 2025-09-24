f5dc2025ccb16914ae823a21ddd96c70143249bc
Your Name <you@example.com>
2025-09-24 23:17:58 +0300

    edit

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,31 +2,33 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *node;
-	unsigned int i = 0;
+    dlistint_t *node;
+    unsigned int i = 0;
 
-	if (!head || !*head)
-		return (-1);
+    if (!head || !*head)
+        return (-1);
 
-	node = *head;
-	while (node && i < index)
-	{
-		node = node->next;
-		i++;
-	}
+    node = *head;
 
-	if (!node) /* index out of range */
-		return (-1);
+    /* امشِ حتى توصل لعقدة الفهرس */
+    while (node && i < index)
+    {
+        node = node->next;
+        i++;
+    }
 
-	/* وصلنا للعقدة المستهدفة: فك الروابط من الجهتين */
-	if (node->prev)
-		node->prev->next = node->next;
-	else
-		*head = node->next; /* حذفنا أول عنصر: حدّث الرأس */
+    if (!node) /* index خارج الحدود */
+        return (-1);
 
-	if (node->next)
-		node->next->prev = node->prev;
+    /* فك الروابط من الجهتين */
+    if (node->prev)
+        node->prev->next = node->next;
+    else
+        *head = node->next; /* حذفنا أول عنصر: حدّث الرأس */
 
-	free(node);
-	return (1);
+    if (node->next)
+        node->next->prev = node->prev;
+
+    free(node);
+    return (1);
 }
